0ed87b98b49c1ac50feeff89703e9292cfb21224
mnafian <mnafian@icloud.com>
2015-12-09 23:04:14 +0700

    Initial commit

---
+++ app/src/main/java/net/mnafian/samplenajib/Question.java
@@ -0,0 +1,75 @@
+package net.mnafian.samplenajib;
+
+/**
+ * Created on : December 09, 2015
+ * Author     : mnafian
+ * Name       : M. Nafian
+ * Email      : mnafian@icloud.com
+ * GitHub     : https://github.com/mnafian
+ * LinkedIn   : https://id.linkedin.com/in/mnafian
+ */
+public class Question {
+    private int ID;
+    private String QUESTION;
+    private String OPTA;
+    private String OPTB;
+    private String OPTC;
+    private String ANSWER;
+    public Question()
+    {
+        ID=0;
+        QUESTION="";
+        OPTA="";
+        OPTB="";
+        OPTC="";
+        ANSWER="";
+    }
+    public Question(String qUESTION, String oPTA, String oPTB, String oPTC,
+                    String aNSWER) {
+
+        QUESTION = qUESTION;
+        OPTA = oPTA;
+        OPTB = oPTB;
+        OPTC = oPTC;
+        ANSWER = aNSWER;
+    }
+    public int getID()
+    {
+        return ID;
+    }
+    public String getQUESTION() {
+        return QUESTION;
+    }
+    public String getOPTA() {
+        return OPTA;
+    }
+    public String getOPTB() {
+        return OPTB;
+    }
+    public String getOPTC() {
+        return OPTC;
+    }
+    public String getANSWER() {
+        return ANSWER;
+    }
+    public void setID(int id)
+    {
+        ID=id;
+    }
+    public void setQUESTION(String qUESTION) {
+        QUESTION = qUESTION;
+    }
+    public void setOPTA(String oPTA) {
+        OPTA = oPTA;
+    }
+    public void setOPTB(String oPTB) {
+        OPTB = oPTB;
+    }
+    public void setOPTC(String oPTC) {
+        OPTC = oPTC;
+    }
+    public void setANSWER(String aNSWER) {
+        ANSWER = aNSWER;
+    }
+
+}
